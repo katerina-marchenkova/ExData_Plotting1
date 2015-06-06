12773033a67982f28a358b1f3f1fd54c6d418de4
katerina-marchenkova <ekaterina.marchenkova@gmail.com>
2015-06-06 20:05:00 +0300

    plot1.R added

---
+++ plot1.R
@@ -0,0 +1,9 @@
+#reads all the data from the dataset
+allData <-read.csv2("household_power_consumption.txt", na.strings="?",  stringsAsFactors=FALSE)
+#subsets the Global Active power for 2 days
+globalActivePower <- allData[(allData$Date== "1/2/2007" | allData$Date=="2/2/2007"), 3]
+globalActivePower <- as.numeric(globalActivePower)
+#creates the histogram
+png("plot1.png", width = 480, height = 480, units = "px")
+hist(globalActivePower, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
+dev.off()
