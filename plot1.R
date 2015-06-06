1b3852e28a6d1453e1a42bb18e323e2379d8079f
katerina-marchenkova <ekaterina.marchenkova@gmail.com>
2015-06-06 22:59:40 +0300

    transparent background added

--- plot1.R
+++ plot1.R
@@ -4,6 +4,6 @@ allData <-read.csv2("household_power_consumption.txt", na.strings="?",  stringsA
 globalActivePower <- allData[(allData$Date== "1/2/2007" | allData$Date=="2/2/2007"), 3]
 globalActivePower <- as.numeric(globalActivePower)
 #creates the histogram
-png("plot1.png", width = 480, height = 480, units = "px")
-hist(globalActivePower, main="Global Active Power", col="red", bg = "transparent", xlab="Global Active Power (kilowatts)")
+png("plot1.png", width = 480, height = 480, units = "px", bg = "transparent")
+hist(globalActivePower, main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)")
 dev.off()
